17f442f0084e920398aba00cc0bb6086521eda67
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2022-10-04 12:24:18 -0500

    made changes to App.js and added assignment component

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -9,21 +9,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Canvas></Canvas>
-        <p>
-          Edit <code>src/App.js</code> and <a href = "https://www.youtube.com/">save to reload</a>.
-        </p>
         <Assignment></Assignment>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        
+      </body>
     </div>
   );
 }
