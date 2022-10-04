d06171fd88a77e69c75dcd2343dc5d5d409d2683
rachelsamojedny <rcs6@illinois.edu>
2022-10-04 12:53:58 -0500

    added addcourse component

---
+++ test-app/src/components/AddCourse.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const AddCourse = ({}) => (
+    <div>
+            <input type="text" placeholder="Link to Course Website.."></input>
+            <input type="text" placeholder="Course Name.."></input>
+            <a href="#home">Add Course</a>
+    </div>
+);
+
+export { AddCourse };
