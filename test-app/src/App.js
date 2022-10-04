5247dd4a2cebba8ae65b918a6cb47781b286a900
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2022-10-04 12:54:04 -0500

    added two assignment compoenents with their features filled

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import employee from './data.json'
-import Assignment from './components/Assignment.tsx'
+import { Assignment } from './components/Assignment.tsx'
 import Canvas from './Canvas.tsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Canvas></Canvas>
-        <Assignment></Assignment>
+        <Assignment name="CS 225" assignment = "MP Lists" time = "11:59" type="MP"></Assignment>
+        <Assignment name="RST 230" assignment = "Quiz 11" time = "10:00" type="Quiz"></Assignment>
       </header>
       <body>
         
