074032870b3535557033ce9b5a7c698ac9a6846f
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2022-10-06 16:40:16 -0500

    fixed the issue with the nav bar colliding with the list of dates/assignments with a few styling changes and also made the styling for assignments a bit cleanear

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -10,26 +10,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div class = "top" style={{backgroundColor: 'lightblue', width: '1000px', height: '150px', borderRadius: '15px', }}>
-          <AddCourse></AddCourse>
-          <AddKeywords></AddKeywords>
-        </div>
-        <div>
-          <DateBox date = "October 5th, 2022"></DateBox>
-            <Assignment name="CS 225" assignment = "MP Lists" time = "11:59 PM" type="MP"></Assignment>
-            <Assignment name="RST 230" assignment = "Quiz 11" time = "10:00 PM" type="Quiz"></Assignment>
-          <DateBox date = "October 6th, 2022"></DateBox>
-            <Assignment name="CS 233" assignment = "Midterm" time = "8:00 AM" type="Exam"></Assignment>
-          <DateBox date = "October 7th, 2022"></DateBox>
-            <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
-        </div> 
-
-      </header>
-      <body>
-        
-      </body>
+      {/* <header className="App-header">
+      </header> */}
+     <div style={{backgroundColor: "red", position: "fixed", top: 0, width: "100%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+          <AddCourse/>
+          <AddKeywords/>
+      </div>
+      <div style={{backgroundColor: "blue", marginTop:"100px", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", paddingTop: "50px"}}>
+       <DateBox date = "October 5th, 2022"></DateBox>
+       <Assignment name="CS 225" assignment = "MP Lists" time = "11:59 PM" type="MP"></Assignment>
+       <Assignment name="RST 230" assignment = "Quiz 11" time = "10:00 PM" type="Quiz"></Assignment>
+     <DateBox date = "October 6th, 2022"></DateBox>
+       <Assignment name="CS 233" assignment = "Midterm" time = "8:00 AM" type="Exam"></Assignment>
+     <DateBox date = "October 7th, 2022"></DateBox>
+       <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
+     <DateBox date = "October 6th, 2022"></DateBox>
+       <Assignment name="CS 233" assignment = "Midterm" time = "8:00 AM" type="Exam"></Assignment>
+     <DateBox date = "October 7th, 2022"></DateBox>
+       <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment> 
+       <DateBox date = "October 7th, 2022"></DateBox>
+       <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
+       <DateBox date = "October 6th, 2022"></DateBox>
+       <Assignment name="CS 233" assignment = "Midterm" time = "8:00 AM" type="Exam"></Assignment>
+     <DateBox date = "October 7th, 2022"></DateBox>
+       <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment> 
+       <DateBox date = "October 7th, 2022"></DateBox>
+       <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
     </div>
+    
+    
+    </div>
+    // add a Todo list component
+    // add menu component
+    // onClick component to make <P> text in assignment strikethrough 
   );
 }
 
