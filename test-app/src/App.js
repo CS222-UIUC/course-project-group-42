c7e307248c04090d288adb850092d640082660ff
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2022-10-07 13:37:13 -0500

    added a basic json file

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -20,18 +20,18 @@ function App() {
        <DateBox date = "October 5th, 2022"></DateBox>
        <Assignment name="CS 225" assignment = "MP Lists" time = "11:59 PM" type="MP"></Assignment>
        <Assignment name="RST 230" assignment = "Quiz 11" time = "10:00 PM" type="Quiz"></Assignment>
-     <DateBox date = "October 6th, 2022"></DateBox>
+       <DateBox date = "October 6th, 2022"></DateBox>
        <Assignment name="CS 233" assignment = "Midterm" time = "8:00 AM" type="Exam"></Assignment>
-     <DateBox date = "October 7th, 2022"></DateBox>
+       <DateBox date = "October 7th, 2022"></DateBox>
        <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
-     <DateBox date = "October 6th, 2022"></DateBox>
+       <DateBox date = "October 6th, 2022"></DateBox>
        <Assignment name="CS 233" assignment = "Midterm" time = "8:00 AM" type="Exam"></Assignment>
-     <DateBox date = "October 8th, 2022"></DateBox>
+       <DateBox date = "October 8th, 2022"></DateBox>
        <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment> 
-      <DateBox date = "October 9th, 2022"></DateBox>
+       <DateBox date = "October 9th, 2022"></DateBox>
        <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
-    <DateBox date = "October 10th, 2022"></DateBox>
-     <DateBox date = "October 11th, 2022"></DateBox>
+       <DateBox date = "October 10th, 2022"></DateBox>
+       <DateBox date = "October 11th, 2022"></DateBox>
        <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment> 
        <DateBox date = "October 12th, 2022"></DateBox>
        <Assignment name="STAT 400" assignment = "Problem Set" time = "11:59 PM" type="HW"></Assignment>
@@ -39,9 +39,15 @@ function App() {
     
     
     </div>
-    // add a Todo list component
-    // add menu component
+
+    // add a ToDo list component
+    // add NavBar component
+    // When user clicks add course 
     // onClick component to make <P> text in assignment strikethrough 
+
+    // parse a JSON Locally
+    // try to host a MongoDB non-relational database
+    // understand how server protocols work (look into it)
   );
 }
 
