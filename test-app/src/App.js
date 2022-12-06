19318c62baf9948ec5c0621b8b9041cbe8311407
rachelsamojedny <rcs6@illinois.edu>
2022-12-05 20:17:46 -0600

    made changes

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -2,7 +2,7 @@ import {AddCourse} from './components/AddCourse.tsx'
 import {AddKeywords} from './components/AddKeywords.tsx'
 import { AssignmentList } from './components/AssignmentList.tsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Popup } from '/Users/rohan_v/Desktop/CS222/course-project-group-42/test-app/src/components/Popup.tsx'
+//import { Popup } from '/Users/rohan_v/Desktop/CS222/course-project-group-42/test-app/src/components/Popup.tsx'
 import { useState } from 'react'
 import './App.css';
 
@@ -14,12 +14,12 @@ function App() {
           <h1 style={{color: "white", fontFamily: "Verdana", marginTop: "10px"}}>Canvas TODO List</h1>
           <AddCourse/>
           {/* <AddKeywords/> */}
-          <button onClick = {() => setButtonPopup(true)}>popup</button>
+          {/* <button onClick = {() => setButtonPopup(true)}>popup</button> */}
       </div>
       <div style={{marginTop:"100px", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", paddingTop: "50px"}}>
         <AssignmentList></AssignmentList>
     </div>
-    <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup>
+    {/* <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup> */}
     
     </div>
   );
--- test-app/src/components/AddCourse.tsx
+++ test-app/src/components/AddCourse.tsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+// import {writeJsonFile} from 'write-json-file';
+
 
 function AddCourse() {
-    const [buttonPopup, setButtonPopup] = useState();
+    // const [message, setMessage] = useState('');
+    const inputRef = useRef(null);
+    function handleClick() {
+        // await writeJsonFile('/Users/rachel/Documents/CS222/course-project-group-42/test-app/src/components/token_stored.json', {token_stored: true});
+        // writeJsonFile('/Users/rachel/Documents/CS222/course-project-group-42/test-app/src/components/token_stored.json', inputRef.current.value)
+        console.log(inputRef.current.value);
+    }
+    // const handleChange = event => {
+    //     setMessage(event.target.value);
+    //     console.log('value is:', event.target.value)
+    // }
+
     return (
         <div style={{ display: "flex", justifyContent: 'space-between', alignItems: "row", borderColor: 'red'}}>
-            <input style={{ backgroundColor: 'orange', width: '550px', height: '40px', borderRadius: '15px' }} type="text" placeholder="Link to Course Website.."></input>
-            <input style={{ backgroundColor: 'orange', width: '250px', height: '40px', borderRadius: '15px', marginBottom: "20px" }} type="text" placeholder="Course Name.."></input>
-            <button>Add Course</button>
+            <input ref={inputRef} type = "text" id = "message" name = "message" style={{ backgroundColor: 'orange', width: '550px', height: '40px', borderRadius: '15px' }}  placeholder="Input API Token"></input>
+            <button  onClick={handleClick}>Add Course</button>
             {/* <button onChange={() => setButtonPopup(true)}>Add Course</button>
             <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup> */}
         </div>
     );
 }
+
+// function GetInput() {
+//     const variable = document.getElementById('input_id');
+//     console.log(variable);
+// }
 
 export { AddCourse };
